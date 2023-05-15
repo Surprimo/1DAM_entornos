645d208ed43f6d7c886a2d566548eca1dd9905a8
Surprimo <alepasdie@alu.edu.gva.es>
2023-05-15 10:27:11 +0200

    Tasca4 con ARRAYS.

--- src/DataType.java
+++ src/DataType.java
@@ -1,3 +1,5 @@
+import java.util.stream.IntStream;
+
 public class DataType {
     public static void main(String[] args) {
         System.out.println(DataType.gd(20));
@@ -7,12 +9,17 @@ public class DataType {
 
     public static String gd(int code) {
 
-        return switch (code) {
-            case 20, 21, 22, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3734, 3769, 12396 ->
-                    "NUMERIC";
-            case 1082, 1083, 1114, 1184, 1266, 12403 -> "DATE";
-            case default -> "STRING";
-        };
+        int numeric[] = {20, 21, 22, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3734, 3769, 12396};
+        int date[] = {1082, 1083, 1114, 1184, 1266, 12403};
+        boolean numericR = IntStream.of(numeric).anyMatch(x -> x == code);
+        boolean dateR = IntStream.of(date).anyMatch(x -> x == code);
+        if (numericR) {
+            return "NUMERIC";
+        } else if (dateR) {
+            return "DATE";
+        } else{
+            return "STRING";
+        }
     }
 
 
